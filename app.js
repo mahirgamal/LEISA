9dd0f5b6c3d03bc0e728ae3b56651e677bafec74
mahirgamal <86919381+mahirgamal@users.noreply.github.com>
2023-09-28 11:05:54 +1000

    test

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.post('/login', (req, res, next) => {
     }
 
     if (!user) {
-      req.flash('error', 'Invalid username or password'); // Set an error flash message
+      req.flash('error', 'Invalid username or password'); // Set an error flash mess
       return res.redirect('/login');
     }
 
